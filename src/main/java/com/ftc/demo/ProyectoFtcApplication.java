460c7326927e070e16cc82c7ae3f474eec72c67f
DennZard <dennzard@gmail.com>
2025-05-09 08:20:47 +0200

    Creacion del Proyecto

---
+++ src/main/java/com/ftc/demo/ProyectoFtcApplication.java
@@ -0,0 +1,13 @@
+package com.ftc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFtcApplication.class, args);
+	}
+
+}
